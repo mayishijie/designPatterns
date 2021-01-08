a99438374c61bddc8540272f169891f8ea069f06
mayishijie <2390504887@qq.com>
2021-01-08 12:58:11 +0800

    工厂类

---
+++ design/src/main/java/com/mayishijie/factory/facotory1/HuMan.java
@@ -0,0 +1,8 @@
+package com.mayishijie.factory.facotory1;
+
+/**
+ * @author tty
+ * @version 1.0 2021-01-08 12:56
+ */
+public class HuMan {
+}
---
+++ design/src/main/java/com/mayishijie/template/Template.java
@@ -0,0 +1,8 @@
+package com.mayishijie.template;
+
+/**
+ * @author tty
+ * @version 1.0 2021-01-08 12:57
+ */
+public class Template {
+}
